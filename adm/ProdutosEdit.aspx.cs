56c9c4646478585e506c03011f58d94f66a40c2c
Murilo Vasconcellos <murilo.vasconsellos@fatec.sp.gov.br>
2017-11-14 11:12:08 -0200

    14-11-17

--- adm/ProdutosEdit.aspx.cs
+++ adm/ProdutosEdit.aspx.cs
@@ -15,17 +15,28 @@ public partial class adm_ProdutosEdit : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack) LoadGrid();
     }
+
+    protected void LoadGrid()
+    {
+        string comando = "SELECT ProdutoId,Nome,Status FROM Produtos WHERE Status>-1 ORDER BY Nome";
+        ole.ConnectionString = conexao;
+        DataTable tb = (DataTable)ole.Query(comando);
+        Produtos.DataSource = tb;
+        Produtos.DataBind();
+    }
+
 
     protected void Buscar_Click(object sender, EventArgs e)
     {
-
+        LoadGrid();
     }
 
     protected void LimparBusca_Click(object sender, EventArgs e)
     {
-
+        BuscarNome.Text = "";
+        LoadGrid();
     }
 
     protected void GridView1_PageIndexChanged(object sender, EventArgs e)
@@ -44,7 +55,7 @@ public partial class adm_ProdutosEdit : System.Web.UI.Page
         // Grava a img do prduto na pasta
         if (FileUpload1.HasFile)
         {
-            UrlFoto.Value = "~/produtos_fotos/" + FileUpload1.PostedFile.FileName;
+            UrlFoto.Value = "~/produtos_fotos/" + FileUpload1.FileName;
             string caminhoFisico = Server.MapPath(UrlFoto.Value);
             // salva a foto
             FileUpload1.SaveAs(caminhoFisico);
@@ -52,23 +63,43 @@ public partial class adm_ProdutosEdit : System.Web.UI.Page
 
         string sql = "";
 
-        if (ProdutoId.Text != "")
+        if (ProdutoId.Text != "") 
         {
             //upload
-            sql = "update";
+            sql = "UPDATE Produtos SET Nome='"+Nome.Text+"',Descricao='"+Descricao.Text+"',UrlFoto='"+UrlFoto.Value+"',Status="+Status.SelectedValue+",DateTimeInsert=Now(),DateTimeUpdate=Now();";
 
         }
         else
         {
             // insert
 
-            sql = "insert";
+            sql = "INSERT INTO Produtos (Nome,Descricao,UrlFoto,Status,DateTimeUpdate) VALUES ('"+Nome.Text+"','"+Descricao.Text+"',UrlFoto='"+UrlFoto.Value+"',DateTimeUpdate=Now();";
         }
 
+        // Conecta no Banco de Dados e Executa o Comando SQL
+        ole.ConnectionString = conexao;
+        ole.Query(sql);
+        Limpar();
+
+    }
+
+    protected void Limpar()
+    {
+        Nome.Text = "";
+        Descricao.Text = "";
+        UrlFoto.Value = "";
+        Status.SelectedValue = "1";
+        Foto.ImageUrl = "";
+        Excluir.Enabled = false;
     }
 
     protected void Excluir_Click(object sender, EventArgs e)
     {
+
+        // Conecta no banco de dados e executao o comando sql
+        ole.ConnectionString = conexao;
+        ole.Query("UPDATE Produtos SET Status=1, DateTimeUpdate=Now()");
+        Limpar();
 
     }
 }
